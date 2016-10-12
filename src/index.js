3582df6f2c17c83ffc014ba519122eac0928235f
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2016-10-12 22:39:19 +0200

    update presets order

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ function createConfig() {
       require.resolve('babel-plugin-transform-decorators-legacy'),
     ],
     presets: [
-      require.resolve('babel-preset-es2015'),
       require.resolve('babel-preset-react'),
+      require.resolve('babel-preset-es2015'),
       require.resolve('babel-preset-stage-0'),
     ],
   };
